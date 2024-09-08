61b2b412b94210323baf239898be9478681f8c19
Chris Sullivan <chrissullivan.dev@gmail.com>
2024-09-07 22:28:13 -0700

    Use non-beta-url

--- src/scripts/environment.sh
+++ src/scripts/environment.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # shellcheck source=/dev/null
 
-export FOUNDATION_API_URL=https://foundation-api.beta.teleology.io/v1/environment
+export FOUNDATION_API_URL=https://foundation-api.teleology.io/v1/environment
 curl -s https://raw.githubusercontent.com/teleology-io/foundation-cli/master/download.sh | bash
 ./foundation --api-key="${PARAM_TO}" environment >> "$BASH_ENV"
 source "$BASH_ENV"
