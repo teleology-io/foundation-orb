ff91a059e01c2e8e67469825a081b4b791efd62f
Chris Sullivan <chrissullivan.dev@gmail.com>
2024-09-08 23:59:08 -0700

    F

--- src/scripts/environment.sh
+++ src/scripts/environment.sh
@@ -2,4 +2,4 @@
 # shellcheck source=/dev/null
 
 curl -s https://raw.githubusercontent.com/teleology-io/foundation-cli/master/download.sh | bash
-./foundation environment >> .env
+./foundation environment >> /tmp/.env
