bece927287b97e9170a913f785ded2926c78c5d3
Chris Sullivan <chrissullivan.dev@gmail.com>
2024-09-08 23:45:17 -0700

    multi-step flow

--- src/scripts/environment.sh
+++ src/scripts/environment.sh
@@ -2,6 +2,5 @@
 # shellcheck source=/dev/null
 
 curl -s https://raw.githubusercontent.com/teleology-io/foundation-cli/master/download.sh | bash
-ls 
 ./foundation environment >> "$BASH_ENV"
-source "$BASH_ENV"
+# source "$BASH_ENV"
