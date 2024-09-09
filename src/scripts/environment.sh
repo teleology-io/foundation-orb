365f6c23045bdf405b2fe1ff57d9ec34af64f3d7
Chris Sullivan <chrissullivan.dev@gmail.com>
2024-09-08 23:17:48 -0700

    output ls to see directory structure

--- src/scripts/environment.sh
+++ src/scripts/environment.sh
@@ -2,5 +2,6 @@
 # shellcheck source=/dev/null
 
 curl -s https://raw.githubusercontent.com/teleology-io/foundation-cli/master/download.sh | bash
+ls 
 ./foundation environment >> "$BASH_ENV"
 source "$BASH_ENV"
